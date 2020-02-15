b420b16773459701eb84f7e451a9c05406c8847d
Tyler Gray <tyler.gray@gmail.com>
2020-02-15 00:02:07 -0600

    Initial commit

---
+++ src/main/java/com/astroviking/avspetclinic/AvsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.astroviking.avspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvsPetClinicApplication.class, args);
+    }
+
+}
