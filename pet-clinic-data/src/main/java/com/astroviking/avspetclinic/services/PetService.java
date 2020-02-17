e30c1f6bc7f9046402c8b72359ada4674cb42976
Tyler Gray <tyler.gray@gmail.com>
2020-02-17 17:22:38 -0600

    Refactored service interfaces, closes #11.

--- pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/PetService.java
@@ -2,13 +2,4 @@ package com.astroviking.avspetclinic.services;
 
 import com.astroviking.avspetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-  Pet findById(Long id);
-
-  Pet save(Pet pet);
-
-  Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
--- pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/VetService.java
@@ -2,13 +2,4 @@ package com.astroviking.avspetclinic.services;
 
 import com.astroviking.avspetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet vet);
-
-  Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long> {}
