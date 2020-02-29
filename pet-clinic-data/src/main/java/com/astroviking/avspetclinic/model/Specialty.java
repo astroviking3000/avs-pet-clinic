324f1dcd80d39a304dfedd4ee55bb919e56870c0
Tyler Gray <tyler.gray@gmail.com>
2020-02-29 14:18:35 -0600

    Added specialty entity, added specialties to vets.  Closes #27

---
+++ pet-clinic-data/src/main/java/com/astroviking/avspetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package com.astroviking.avspetclinic.model;
+
+public class Specialty extends BaseEntity {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
