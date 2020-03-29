3419bc27a5fbc4416f976a7f984246f39e39e39e
Tyler Gray <tyler.gray@gmail.com>
2020-03-29 11:15:45 -0500

    Owner find functionality, closes #54

--- pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.astroviking.avspetclinic.services;
 
 import com.astroviking.avspetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
   Owner findByLastName(String lastName);
+
+  List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/astroviking/avspetclinic/services/map/OwnerServiceMap.java
@@ -8,6 +8,7 @@ import com.astroviking.avspetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -76,5 +77,11 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         .filter(entry -> entry.getLastName().equals(lastName))
         .findFirst()
         .orElse(null);
+  }
+
+  @Override
+  public List<Owner> findAllByLastNameLike(String lastName) {
+    // todo - impl
+    return null;
   }
 }
